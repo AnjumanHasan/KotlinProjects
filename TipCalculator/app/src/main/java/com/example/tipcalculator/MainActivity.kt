79d2277beff7dd10fbed556940c7662de7b228da
Anjuman Hasan <anju2084@gmail.com>
2022-02-07 02:46:42 +0530

    Added seekbar functions

--- TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,66 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+private val initialTip = 0
+class MainActivity : AppCompatActivity()  {
+    lateinit var seekBar :SeekBar
+    lateinit var bill : EditText
+    lateinit var tipText : TextView
+    lateinit var tip :TextView
+    lateinit var total :TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            seekBar =findViewById(R.id.seekBar)
+            bill= findViewById(R.id.billTotal)
+            tipText = findViewById(R.id.textTip)
+            tip = findViewById(R.id.tipValue)
+            total  =findViewById(R.id.tipValue)
+
+        seekBar.progress = initialTip
+        tipText.text ="$initialTip%"
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                tipText.text = "$p1%"
+
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+        bill.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+             computeTipandTotal()
+            }
+
+        })
     }
+
+    private fun computeTipandTotal() {
+        val amount = bill.text.toString().toDouble()
+        val tip = seekBar.progress
+
+        val tipValue= amount*tip/100
+        val total = amount + tipValue
+        
+    }
+
+
 }
