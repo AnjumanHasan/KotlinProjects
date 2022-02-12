1c1ee8f64ff40f8b017bf50bf80e81e25f217ca4
Anjuman Hasan <anju2084@gmail.com>
2022-02-13 02:17:55 +0530

    updated main acitvity

--- MatchingGame/app/src/main/java/kotlinprojects/matchinggame/MainActivity.kt
+++ MatchingGame/app/src/main/java/kotlinprojects/matchinggame/MainActivity.kt
@@ -2,10 +2,16 @@ package kotlinprojects.matchinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinprojects.matchinggame.R.drawable.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val images : MutableList<Int> = mutableListOf(cottoncandy,cupcake,donut,icecream,pancake,pudding)
+        val buttons = arrayOf(R.id.button,R.id.button2,R.id.button3,R.id.button4,R.id.button5,
+                R.id.button6,R.id.button7,R.id.button8,R.id.button9,R.id.button10,R.id.button11,R.id.button12)
     }
 }
