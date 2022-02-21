902d63aa16f68df5f44f8a3bb2ad356921b86c7f
Anjuman Hasan <anju2084@gmail.com>
2022-02-22 01:44:45 +0530

    changed buttons to imagebuttons

--- MatchingGame/app/src/main/java/kotlinprojects/matchinggame/MainActivity.kt
+++ MatchingGame/app/src/main/java/kotlinprojects/matchinggame/MainActivity.kt
@@ -1,9 +1,7 @@
 package kotlinprojects.matchinggame
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -17,39 +15,33 @@ class MainActivity : AppCompatActivity() {
         var button2: ImageButton = findViewById(R.id.button2)
         var button3: ImageButton = findViewById(R.id.button3)
         var button4: ImageButton = findViewById(R.id.button4)
-        var button5: ImageButton = findViewById(R.id.button5)
         var button6: ImageButton = findViewById(R.id.button6)
         var button7: ImageButton = findViewById(R.id.button7)
         var button8: ImageButton = findViewById(R.id.button8)
+        var button5: ImageButton = findViewById(R.id.button5)
         var button9: ImageButton = findViewById(R.id.button9)
         var button10: ImageButton = findViewById(R.id.button10)
         var button11: ImageButton = findViewById(R.id.button11)
         var button12: ImageButton = findViewById(R.id.button12)
 
-        val images : MutableList<Int> = mutableListOf(cottoncandy,cupcake,donut,icecream,pancake,pudding,
+        val images : MutableList<Int> = mutableListOf(cottoncandy,donut,icecream,pancake,pudding,cupcake,
             cottoncandy,cupcake,donut,icecream,pancake,pudding)
         val buttons : Array<ImageButton> = arrayOf(button1,button2,button3,button4,button5,button6,button7,button8,
         button9,button10,button11,button12)
-        val cardBack : Int = help
+
 
         images.shuffle()
-        for (i in 0..11) {
+        buttons.indices.map { index ->
+            MemoryCard(images[index],false,false)
+        }
 
-            buttons[i].textDirection = 1
-        }
-        for (i in 0..11)
-        {
-            buttons[i].setOnClickListener{
-                if (buttons[i].textDirection == 1)
-                {
-                    buttons[i].setImageResource(images[i])
-                    buttons[i].textDirection = 0
-                }
-                else if (buttons[i].textDirection != 1)
-                {
-                    buttons[i].setImageResource(cardBack)
-                }
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                button.setBackgroundResource(images[index])
+
             }
+
         }
+
     }
 }
---
+++ MatchingGame/app/src/main/java/kotlinprojects/matchinggame/MemoryCard.kt
@@ -0,0 +1,4 @@
+package kotlinprojects.matchinggame
+
+data class MemoryCard (val identifier :Int,var isFaceUp : Boolean, var isMatched : Boolean)
+
