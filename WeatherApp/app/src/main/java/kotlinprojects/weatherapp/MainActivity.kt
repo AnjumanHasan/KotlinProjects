21e8de43d7aecd8f2d709e03070beb220cabbb83
Anjuman Hasan <anju2084@gmail.com>
2022-03-05 12:10:13 +0530

    Adding main.kt

--- WeatherApp/app/src/main/java/kotlinprojects/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/kotlinprojects/weatherapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package kotlinprojects.weatherapp
 
+import android.os.AsyncTask
+import android.os.AsyncTask.execute
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    val city : String = "New Delhi,IN"
+    val API : String = "e7919300e189ee64e9624a1d94739939"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
     }
+
+    inner class weatherTask () : AsyncTask<String, Void, String>()
+    {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility =  View. VISIBLE
+            findViewById<ConstraintLayout>(R.id.maincontainer).visibility = View.GONE
+            findViewById<TextView>(R.id.errortext).visibility = View.GONE
+
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+
+        }
+    }
+
 }
